1d00b47fbe839295ba698b140e8a2cdb3c0aa6bb
HungTnq <trannguyenquochung00@gmail.com>
2024-04-22 11:58:46 +0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+<<<<<<< HEAD
 # Task-management
+=======
+# flutter_task_management_project
+
+A new Flutter project.
+>>>>>>> 0c9167d (Initial commit)
---
+++ android/app/src/main/kotlin/com/example/flutter_task_management_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_task_management_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
